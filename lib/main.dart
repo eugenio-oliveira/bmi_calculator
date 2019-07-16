943cb589cd2cf2e211d66fe9f80607f819f3a86c
unknown <Eugnio Oliveira>
2019-07-16 19:47:58 -0300

    scrollView and raisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -28,33 +28,52 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_pin, size: 120.0, color: Colors.blue),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Weight: (KG)",
-                labelStyle: TextStyle(color: Colors.blue)),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Height: (CM)",
-                labelStyle: TextStyle(color: Colors.blue)),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold),
-          )
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_pin, size: 120.0, color: Colors.blue),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Weight: (KG)",
+                  labelStyle: TextStyle(color: Colors.blue)),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Height: (CM)",
+                  labelStyle: TextStyle(color: Colors.blue)),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                    onPressed: () {},
+                    child: Text("Calculate BMI",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                    color: Colors.blue),
+              ),
+            ),
+            Text(
+              "Result: ",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blue, fontSize: 20.0),
+            ),
+          ],
+        ),
       ),
     );
   }
